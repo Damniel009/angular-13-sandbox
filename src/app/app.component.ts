cb7a73fb94411e596bfa4070c45a388527ac894a
Damniel009 <vargadaniel28@users.noreply.github.com>
2021-11-09 12:44:30 +0200

    Added bootstrap5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,15 @@ export class AppComponent {
   title = 'angular13';
   param = { value: 'world' };
 
-  constructor(translate: TranslateService) {
+  constructor(private translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('ro-RO');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('ro-RO');
+  }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
   }
 }
