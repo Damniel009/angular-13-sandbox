d060d3ee5019f2f7c62a30c8800141fa007cc6c9
Damniel009 <vargadaniel28@users.noreply.github.com>
2021-11-09 12:13:22 +0200

    Added basic use of translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';   
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {
+  TranslateModule,
+  TranslateLoader,
+  TranslateService,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { registerLocaleData } from '@angular/common';
+
+import localeEn from '@angular/common/locales/en';
+import localeHu from '@angular/common/locales/hu';
+import localeRo from '@angular/common/locales/ro';
+
+registerLocaleData(localeEn, localeHu, localeRo);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TranslateModule.forRoot({
       loader: {
@@ -23,10 +33,11 @@ import { AppRoutingModule } from '../app-routing.module';
       },
     }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [TranslateService],
+  bootstrap: [AppComponent],
+  exports: [TranslateModule]
 })
-export class AppModule { }
+export class AppModule {}
 
 export function translateLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
